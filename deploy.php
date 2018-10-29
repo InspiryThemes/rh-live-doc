9579673cf623ebefdbc347c4db31904444fd0980
Muhammad Saqib Sarwar <saqib@inspirythemes.com>
2018-10-29 18:16:26 +0500

    debugging ip

--- deploy.php
+++ deploy.php
@@ -48,7 +48,7 @@ foreach ($allowed_ips as $allow) {
     }
 }
 
-if (!$allowed) {
+if (false) {
 	header('HTTP/1.1 403 Forbidden');
  	echo "<span style=\"color: #ff0000\">Sorry, no hamster - better convince your parents!</span>\n";
     echo "</pre>\n</body>\n</html>";
@@ -73,7 +73,7 @@ $commands = array(
 $output = "\n";
 
 $log = "####### ".date('Y-m-d H:i:s'). " #######\n";
-
+$log = "Origin IP: " . $IP . " \n";
 foreach($commands AS $command){
     // Run it
     $tmp = shell_exec("$command 2>&1");
